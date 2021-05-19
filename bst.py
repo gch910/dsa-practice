39bfb35c436db23f3ad4b8ade08b2e13b32bbbd6
Gabriel Harris <gabrielharris@Gabriels-MBP.attlocal.net>
2021-05-19 17:32:05 -0500

    bst

---
+++ bst.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+class BST:
+    def __init__(self):
+        self.root = None
+    
+    def insert(self, val):
+        if not self.root:
+            self.root = Node(val)
+            return
+        currentNode = self.root
+        while currentNode:
+            if val < currentNode.val:
+                if currentNode.left:
+                    currentNode = currentNode.left
+                else:
+                    currentNode.left = Node(val)
+                    return
+            else:
+                if currentNode.right:
+                    currentNode = currentNode.right
+                else:
+                    currentNode.right = Node(val)
+                    return
+    def find(self, val):
+        if not self.root:
+            return False
+        currentNode = self.root
+        while currentNode:
+            if currentNode.val == val:
+                return True
+            if val < currentNode.val:
+                currentNode = currentNode.left
+            else:
+                currentNode = currentNode.right
+        return False
+                
+
+tree = BST()
+
+tree.insert(12)
+tree.insert(23)
+tree.insert(9)
+tree.insert(15)
+tree.insert(50)
+tree.insert(10)
+print(tree.find(72))
+
+        
--- dll.py
+++ dll.py
@@ -75,6 +75,39 @@ class Dll:
         self.head = newHead
         self.length += 1
         return newHead.val
+    
+    def remove_start(self):
+        oldHead = self.head
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+            self.length -= 1
+            return oldHead
+        newHead = oldHead.next
+        oldHead.next = None
+        newHead.prev = None
+        self.length -= 1
+        self.head = newHead
+        return oldHead
+
+    def get(self, idx):
+        if idx < 0 or idx >= self.length:
+            return -1
+        if idx == 0 and self.length >= 1:
+            return self.head.val
+        if idx == self.length - 1 and self.length > 1:
+            return self.tail.val
+        else:
+            current_idx = 0
+            current_node = self.head
+            while current_idx < self.length:
+                if idx == current_idx:
+                    return current_node.val
+                current_idx += 1
+                current_node = current_node.next
+
+        
+        
 
          
         
@@ -87,13 +120,16 @@ doubly.push(11)
 doubly.push(12)
 doubly.insert_start(9)
 doubly.insert_start(8)
+doubly.remove_start()
+
 # doubly.push(13)
 # doubly.push(15)
 print(doubly.length)
 print(doubly.tail.val) 
-print(doubly.head.val) 
+print(doubly.head.val)
 print(doubly.head.next.val) 
 print(doubly.head.next.prev.val) 
+print(doubly.get(6))
 
 
 
