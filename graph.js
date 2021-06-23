f399835b90e7514d3356d2500110183a77acc0b5
Gabriel Harris <gch910@gmail.com>
2021-06-23 18:14:52 -0500

    remove vertex

--- graph.js
+++ graph.js
@@ -18,6 +18,11 @@ class Graph {
         this.adjList[v1] = this.adjList[v1].filter(val => val !== v2)
         this.adjList[v2] = this.adjList[v2].filter(val => val !== v1)
     }
+
+    removeVertex(name) {
+        this.adjList[name].forEach(val => this.removeEdge(name, val));
+        delete this.adjList[name];
+    }
 }
 
 const node1 = new Node('Hello');
@@ -31,9 +36,11 @@ const graph = new Graph();
 graph.addVertex(node1.value);
 graph.addVertex(node2.value);
 graph.addVertex(node3.value);
+graph.addEdge(node2.value, node3.value);
 graph.addEdge(node1.value, node2.value)
 graph.addEdge(node1.value, node3.value);
-graph.removeEdge(node1.value, node3.value)
+
+graph.removeVertex(node1.value)
 
 
 console.log(graph.adjList)
